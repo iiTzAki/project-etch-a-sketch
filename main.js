6f5d254f36b6ed297321b6818de0172cdc6baffa
Aki <210830810+iiTzAki@users.noreply.github.com>
2026-01-05 15:19:42 +0500

    add and refactor: event listener for colors
    Change to Dynamic colors depending on
    dynamic color button and some placement
    changes

--- main.js
+++ main.js
@@ -1,16 +1,35 @@
 const getGridContainer = document.querySelector(".grid-container");
 
 const getResetPadBtn = document.querySelector("#reset-button");
-getResetPadBtn.addEventListener("click", () => { 
+getResetPadBtn.addEventListener("click", () => {
+    isColorPenTrue = false; 
     getGridContainer.innerHTML = ""; 
     createGridDivs(16);
 })
 
+let isColorPenTrue = false;
+
+const getDynamicColorBtn = document.querySelector("#dynamic-color-btn");
+getDynamicColorBtn.addEventListener("click", () => { 
+    isColorPenTrue = !isColorPenTrue; 
+})
+
+getGridContainer.addEventListener("mouseover", (event) => {
+
+    if (isColorPenTrue === true && event.target.classList.contains("grid-box")) { 
+        const randomColor = () => Math.floor(Math.random() * 256);
+        event.target.style["background-color"] = `rgb(${randomColor()}, ${randomColor()}, ${randomColor()})`;
+
+    } else if (event.target.classList.contains("grid-box")) { 
+        event.target.style["background-color"] = "black";
+    };
+});
+
 const getResetGridBtn = document.querySelector("#new-grid-button");
 getResetGridBtn.addEventListener("click", () => {
-    const squareVal = Number(prompt("Reset the pad: Pick a number between 1-100")); 
+    const squareVal = parseInt(prompt("Reset the pad: Pick a number between 1-100")); 
 
-    if (squareVal <= 0 || squareVal >= 100) {
+    if (squareVal <= 0 || squareVal > 100) {
         alert("Pick a number between 1-100 for sketch pad.")
         return;
     }
@@ -33,13 +52,9 @@ function createGridDivs(squareVal) {
         newGridDivs.style['border'] = "0.1px solid black"; 
         getGridContainer.appendChild(newGridDivs);
     };
-
-    getGridContainer.addEventListener("mouseover", (event) => { 
-        if (event.target.classList.contains("grid-box")) { 
-            event.target.style["background-color"] = "black"
-        }
-    })
 };
+
+
 
 createGridDivs(16);
 
